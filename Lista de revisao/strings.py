63aafc11a0de0c5588463694eec7d471b9a4d9b0
anahassis <ana.assis1@icloud.com>
2026-06-22 15:32:15 -0300

    Create strings.py

---
+++ Lista de revisao/strings.py
@@ -0,0 +1,26 @@
+# # Questao 1 
+# x = input("Digite seu primeiro nome: ")
+# y = input("Digite seu sobrenome nome: ")
+# print(f"Bem-vinda(o), {x} {y}!")
+
+
+# # Questao 2 
+# x = input(" Digite uma frase: ")
+# y = x.count (" ")
+# print (f"Contagem de espaços vazios: {y}")
+
+# # Questao 3 
+# nome = input("Digite seu nome: ")
+# texto = ""
+# for letra in nome:
+#     texto += letra
+#     print(texto)
+
+# # Questao 4 
+
+# Questao 5
+letra = input("Digite uma frase: ")
+y =  letra in "aeiou"
+z = letra.count("a") + letra.count("e") + letra.count("i") + letra.count("o") + letra.count("u")
+print(f"Indice das vogais: {y}")
+print(f"Número de vogais: {z}")
