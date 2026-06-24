843ae44d9b9718940680e84d2daaa9b6b207e5f6
anahassis <ana.assis1@icloud.com>
2026-06-24 16:04:20 -0300

    Update strings.py

--- Lista de revisao/strings.py
+++ Lista de revisao/strings.py
@@ -1,22 +1,22 @@
-# # Questao 1 
-# x = input("Digite seu primeiro nome: ")
-# y = input("Digite seu sobrenome nome: ")
-# print(f"Bem-vinda(o), {x} {y}!")
+# Questao 1 
+x = input("Digite seu primeiro nome: ")
+y = input("Digite seu sobrenome nome: ")
+print(f"Bem-vinda(o), {x} {y}!")
 
 
-# # Questao 2 
-# x = input(" Digite uma frase: ")
-# y = x.count (" ")
-# print (f"Contagem de espaços vazios: {y}")
+# Questao 2 
+x = input(" Digite uma frase: ")
+y = x.count (" ")
+print (f"Contagem de espaços vazios: {y}")
 
-# # Questao 3 
-# nome = input("Digite seu nome: ")
-# texto = ""
-# for letra in nome:
-#     texto += letra
-#     print(texto)
+# Questao 3 
+nome = input("Digite seu nome: ")
+texto = ""
+for letra in nome:
+    texto += letra
+    print(texto)
 
-# # Questao 4 
+# Questao 4 
 
 # Questao 5
 letra = input("Digite uma frase: ")
